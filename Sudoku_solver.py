3b39bd3521931e3c74ae8615c4b739c585cef787
DavidHernandez24 <146795704+DavidHernandez24@users.noreply.github.com>
2023-10-02 21:35:53 -0600

    Simple Sudoku Solver
    I designed and created a Sudoku solver, employing programming, efficient algorithms, deductive logic, and optimization techniques. The project was divided into three key parts: first, I optimized the Sudoku visualization in the terminal; then, I implemented the logic to fill in the empty spaces validly; finally, I utilized the backtracking method to ensure correct solutions. This project highlights my ability to tackle complex problems and my programming expertise.

---
+++ Sudoku_solver.py
@@ -0,0 +1,56 @@
+sudoku_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print()
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+
+    row_start, col_start = 3 * (row // 3), 3 * (col // 3)
+    for i in range(row_start, row_start + 3):
+        for j in range(col_start, col_start + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+if solve_sudoku(sudoku_board):
+    print("Solved Sudoku:")
+    print_board(sudoku_board)
+else:
+    print("No solution exists.")
